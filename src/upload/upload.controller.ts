fd557fc7672c534813fabae8bb704e0a7bd65b57
alvarogalhardo <alvaromgfernandes@gmail.com>
2025-06-20 17:27:53 -0300

    feat: upload functional fix: lint errors

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -36,7 +36,7 @@ export class UploadController {
     )
     file: Express.Multer.File,
     @Body('storageType') storageType?: string,
-  ) {
+  ): Promise<void> {
     const storage = (storageType as StorageType) || StorageType.LOCAL;
     const fileStorage = this.fileStorageFactory.create(storage);
 
@@ -48,7 +48,6 @@ export class UploadController {
     console.log(
       `Video uploaded successfully: ${fileName} using ${storage} storage`,
     );
-
     return;
   }
 }
